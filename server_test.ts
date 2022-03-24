bc7728467af55de438bd0ae1453f6119627dcf90
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-24 16:48:04 -0500

    :white_check_mark: Add tests

--- server_test.ts
+++ server_test.ts
@@ -1,6 +1,6 @@
 import { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
 
-import { isLeapYear } from "./deps.ts";
+import { isLeapYear, leapYearsInRange } from "./deps.ts";
 
 import { app } from "./app.ts";
 
@@ -30,5 +30,11 @@ Deno.test("Main", async (t) => {
   });
 });
 
-Deno.test("In range", () => {
+Deno.test("In range", async (t) => {
+  await t.step("Library Case", async () => {
+    const request = await superoak(app);
+    await request
+      .get("/range/1/20")
+      .expect(`{"result":${leapYearsInRange({ min: 1, max: 20 })}}`);
+  });
 });
