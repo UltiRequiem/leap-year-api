2eda5fedeb2d3e4bc270c7c466a838d8f1887da9
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-26 16:27:23 -0500

    :green_heart: Fix CI

--- app.ts
+++ app.ts
@@ -27,9 +27,9 @@ router.get("/range/:start/:end", (context) => {
 
 // simple cors
 app.use((ctx, next) => {
-  ctx.response.headers.set('Access-Control-Allow-Origin', '*')
-  return next()
-})
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+  return next();
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
