288d01751a61508f2389d5d7d77ee70b3fe6fb06
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-07 13:43:55 -0500

    :recycle: RouterMiddleware

--- deps.ts
+++ deps.ts
@@ -2,7 +2,4 @@ export { Application, Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
 export { isLeapYear } from "https://deno.land/x/leap_year@v2.1.0/mod.ts";
 export { leapYearsInRange } from "https://deno.land/x/leap_years_utils@0.1.0/mod.ts";
 
-export type {
-  Middleware,
-  RouterContext,
-} from "https://deno.land/x/oak@v10.5.1/mod.ts";
+export type { RouterMiddleware } from "https://deno.land/x/oak@v10.5.1/mod.ts";
--- middlewares.ts
+++ middlewares.ts
@@ -1,26 +1,21 @@
-import {
-  isLeapYear,
-  leapYearsInRange,
-  type Middleware,
-  type RouterContext,
-} from "./deps.ts";
+import { isLeapYear, leapYearsInRange, type RouterMiddleware } from "./deps.ts";
 
-export const RootMiddleware: Middleware = (ctx) => {
+export const RootMiddleware: RouterMiddleware<"/"> = (ctx) => {
   ctx.response.body = { result: isLeapYear() };
 };
 
-export const DocsMiddleware: Middleware = (context) => {
+export const DocsMiddleware: RouterMiddleware<"/docs"> = (context) => {
   context.response.redirect(
     "https://github.com/UltiRequiem/leap-year-api#endpoints"
   );
 };
 
-export const YearMiddleware = (context: RouterContext<"/:year">) => {
+export const YearMiddleware: RouterMiddleware<"/:year"> = (context) => {
   context.response.body = { result: isLeapYear(context.params.year) };
 };
 
-export const RangeMiddleware = (
-  context: RouterContext<"/range/:start/:end">
+export const RangeMiddleware: RouterMiddleware<"/range/:start/:end"> = (
+  context
 ) => {
   context.response.body = {
     result: leapYearsInRange({
